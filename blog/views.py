db25aa33d2bfab4d36170055212cc28d84cc1bee
ParkJongMin <honeysehee@gmail.com>
2020-04-07 00:04:40 +0900

    Post Detail

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
-class PostList(ListView):                          #포스트 리스트로 보여줌
+
+class PostList(ListView):
     model = Post
 
-    def get_queryset(self):                         #포스트 역순으로
+    def get_queryset(self):
         return Post.objects.order_by('-created')
+
+
+class PostDetail(DetailView):
+    model = Post
+
+# def post_detail(request, pk):
+#     blog_post = Post.objects.get(pk=pk)
+#
+#
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'blog_post': blog_post,
+#         }
+#     )
 
 # def index(request):
 #     posts = Post.objects.all()
