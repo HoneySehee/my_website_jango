1ba982cbbce3e9987b5af6da93e20ba26aafc254
ParkJongMin <honeysehee@gmail.com>
2020-04-07 10:40:17 +0900

    initilized post_detail test

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
@@ -12,5 +13,8 @@ class Post(models.Model):
 
     def __str__(self):
         return '{}'.format(self.title)
+
+    def get_absolute_url(self):
+        return '/blog/{}/'.format(self.pk)
 
 
--- blog/tests.py
+++ blog/tests.py
@@ -4,10 +4,26 @@ from .models import Post
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+def create_post(title, content, author):
+    blot_post = Post.objects.create(
+        title=title,
+        content=content,
+        created=timezone.now(),
+        author=author,
+    )
+
+    return blot_post
+
+
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
         self.author_000 = User.objects.create(username='smith', password ='nopass')
+
+    def check_navbar(self,soup):
+        navbar = soup.find('div', id='navbar')
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
 
     def test_post_list(self):
         response = self.client.get('/blog/')
@@ -18,19 +34,20 @@ class TestView(TestCase):
 
         self.assertEqual(title.text, 'Blog')
 
-        navbar = soup.find('div', id='navbar')
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+        self.check_navbar(soup)
+        # navbar = soup.find('div', id='navbar')
+        # self.assertIn('Blog', navbar.text)
+        # self.assertIn('About Me', navbar.text)
 
         self.assertEqual(Post.objects.count(), 0)
         self.assertIn('아직 게시물이 없습니다', soup.body.text)
 
-        post_000 = Post.objects.create(
-            title = 'The First post',
-            content = 'show me the money',
-            created = timezone.now(),
-            author = self.author_000,
+        post_000 = create_post(
+            title='The First post',
+            content='show me the money',
+            author=self.author_000,
         )
+
 
         self.assertGreater(Post.objects.count(), 0)
 
@@ -40,4 +57,25 @@ class TestView(TestCase):
         body = soup.body
         self.assertNotIn('아직 게시물이 없습니다', body.text)
         self.assertIn(post_000.title,body.text)
-    
+
+    def test_post_detail(self):
+        post_000 = create_post(
+            title='The First post',
+            content='show me the money',
+            author=self.author_000,
+        )
+
+        self.assertGreater(Post.objects.count(), 0)
+        post_000_url = post_000.get_absolute_url()
+        self.assertEqual(post_000_url, '/blog/{}/'.format(post_000.pk))
+
+        response = self.client.get(post_000_url)
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title = soup.title
+
+        self.assertEqual(title.text, '{} - Blog'.format(post_000.title))
+
+        self.check_navbar(soup)
+
