edcf15ad031177660d0b9f35b7a31c4b6c25f63e
ParkJongMin <honeysehee@gmail.com>
2020-04-07 21:17:35 +0900

    category in post_list

--- blog/tests.py
+++ blog/tests.py
@@ -1,31 +1,70 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
-from .models import Post
+from .models import Post, Category
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-def create_post(title, content, author):
+
+def create_category(name='life', description=''):
+    category, is_created = Category.objects.get_or_create(
+        name=name,
+        description=description
+    )
+
+    return category
+
+
+def create_post(title, content, author, category=None):
     blot_post = Post.objects.create(
         title=title,
         content=content,
         created=timezone.now(),
         author=author,
+        category=category,
     )
 
     return blot_post
 
 
+class Testmodel(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.author_000 = User.objects.create(username='smith', password='nopass')
+
+    def test_category(self):
+        category = create_category()
+
+        post_000 = create_post(
+            title='The First post',
+            content='show me the money',
+            author=self.author_000,
+            category=category
+        )
+
+        self.assertEqual(category.post_set.count(), 1)
+
+    def test_post(self):
+        category = create_category()
+
+        post_000 = create_post(
+            title='The First post',
+            content='show me the money',
+            author=self.author_000,
+            category=category
+        )
+
+
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
-        self.author_000 = User.objects.create(username='smith', password ='nopass')
+        self.author_000 = User.objects.create(username='smith', password='nopass')
 
     def check_navbar(self,soup):
         navbar = soup.find('div', id='navbar')
         self.assertIn('Blog', navbar.text)
         self.assertIn('About Me', navbar.text)
 
-    def test_post_list(self):
+    def test_post_list_no_post(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
 
@@ -39,10 +78,19 @@ class TestView(TestCase):
         self.assertEqual(Post.objects.count(), 0)
         self.assertIn('아직 게시물이 없습니다', soup.body.text)
 
+    def test_post_list_with_post(self):
+
         post_000 = create_post(
             title='The First post',
             content='show me the money',
             author=self.author_000,
+        )
+
+        post_001 = create_post(
+            title='The Second post',
+            content='show me the money Second',
+            author=self.author_000,
+            category=create_category(name='정치/사회')
         )
 
         self.assertGreater(Post.objects.count(), 0)
@@ -56,6 +104,17 @@ class TestView(TestCase):
 
         post_000_read_more_btn = body.find('a', id='read-more-post-{}'.format(post_000.pk))
         self.assertEqual(post_000_read_more_btn['href'], post_000.get_absolute_url())
+
+        # category card 에서
+        category_card = body.find('div', id='category-card')
+        self.assertIn('미분류 (1)', category_card.text)
+        self.assertIn('정치/사회 (1)', category_card.text)
+
+        # main_div에는
+        main_div = body.find('div', id='main_div')
+        self.assertIn('정치/사회', main_div.text) ####첫번째 포스트에는 '정치/사회' 있어야
+        self.assertIn('미분류', main_div.text) #### 두번째 포스트에는 '미분류' 있어야 함
+
 
     def test_post_detail(self):
         post_000 = create_post(
