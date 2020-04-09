0f5fd7e48f6671d5e71a5ea7ad974ca718e59a63
ParkJongMin <honeysehee@gmail.com>
2020-04-10 01:27:01 +0900

    comment on post detail page

--- blog/models.py
+++ blog/models.py
@@ -61,4 +61,7 @@ class Comment(models.Model):
     text = MarkdownxField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def get_markdown_content(self):
+        return markdown(self.text)
+
 
